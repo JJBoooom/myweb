70dff673f4c231d9edbe260a5d75088a8fbd7251
kiongf <kiongf@126.com>
2016-02-24 20:06:26 +0800

    使用gorilla/mux实现url路由 1.实现简化版的notfoundHandler 2.实现文件的上传和显示文件功能

---
+++ gorilla_mux.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+)
+
+const (
+	storagePath = "/tmp/test"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method)
+	t, _ := template.ParseFiles("goupload.gtpl")
+	t.Execute(w, t)
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("ready to upload")
+	r.ParseMultipartForm(32 << 20)
+	file, handler, err := r.FormFile("uploadfile")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	err = os.MkdirAll(storagePath, 0755)
+	if err != nil {
+		if !os.IsExist(err) {
+			fmt.Println("can't mkdir")
+			return
+		}
+	}
+
+	path := storagePath + "/" + handler.Filename
+
+	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	io.Copy(f, file)
+}
+
+func DownloadHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	//获取url传递的参数
+	filename := vars["filename"]
+	filepath := storagePath + "/" + filename
+
+	_, err := os.Stat(filepath)
+	if os.IsNotExist(err) {
+		fmt.Printf("%s doesn't exist", filename)
+		return
+	}
+	//返回指定路径文件或者目录的内容
+	http.ServeFile(w, r, filepath)
+
+}
+
+type NotFoundHandler struct {
+}
+
+func (NotFoundHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("不见鸟~~"))
+}
+
+func main() {
+	//新增一个web url路由
+	r := mux.NewRouter()
+	var notfound NotFoundHandler
+	r.NotFoundHandler = notfound
+	r.HandleFunc("/", HomeHandler).Methods("GET")
+	r.HandleFunc("/", UploadHandler).Methods("POST")
+	//这里将/后面的内容当做filename参数传递给DownloadHandler
+	//多个参数怎么传递?
+	r.HandleFunc("/{filename}", DownloadHandler).Methods("GET")
+	//使用自己写的路由
+	err := http.ListenAndServe(":9090", r)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
